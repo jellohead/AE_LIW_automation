5e5d07a14912b87596dc460ecb394076d633bd80
Patricio Escalona Jr patescalona@Pats-Mac_mini.local <pat.escalona7@gmail.com>
2025-02-11 07:30:35 -0600

    slide_24.py updates slide 24 correctly.
    Took 10 minutes

--- src/AE_LIW_automation/slide_updaters/__init__.py
+++ src/AE_LIW_automation/slide_updaters/__init__.py
@@ -3,7 +3,8 @@ __all__: list = ['slide_1_updater',
                  'slide_4_updater',
                  'slide_6_updater',
                  'slide_14_updater',
-                 'slide_17_updater'
+                 'slide_17_updater',
+                 'slide_24_updater'
                  ]
 
 
@@ -13,3 +14,4 @@ from .slide_4 import slide_4_updater
 from .slide_6 import slide_6_updater
 from .slide_14 import slide_14_updater
 from .slide_17 import slide_17_updater
+from .slide_24 import slide_24_updater
