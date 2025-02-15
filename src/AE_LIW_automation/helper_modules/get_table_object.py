52dc43668f80b648562f7e742950c64bbb1f0fc6
Patricio Escalona Jr patescalona@Pats-Mac_mini.local <pat.escalona7@gmail.com>
2025-02-15 10:37:03 -0600

    slide_1.ipynb working on adding new slide 8 table, deleting old table
    Took 57 minutes

--- src/AE_LIW_automation/helper_modules/get_table_object.py
+++ src/AE_LIW_automation/helper_modules/get_table_object.py
@@ -8,5 +8,6 @@ def get_table_object(slide: object) -> object:
     table_shape: object = None
     for shape in slide.shapes:
         if shape.has_table:
+            print(f'{dir(shape) = }')
             table_shape = shape.table
             return table_shape
